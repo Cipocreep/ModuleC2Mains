77572cd2ee85e5001e5223b6e29d7c1809166a14
ocollomb <ocollomb@f0r8s19.paris.42.school>
2026-09-13 18:58:35 +0200

    well, might as well do that

--- mainex0.c
+++ mainex0.c
@@ -7,10 +7,16 @@ int		countknockoff(int num);
 int	main(void)
 {
 	ft_putnbr(3);
+	write(1, " ", 1);
 	ft_putnbr(-3);
+	write(1, " ", 1);
 	ft_putnbr(0);
+	write(1, " ", 1);
 	ft_putnbr(-32345);
+	write(1, " ", 1);
 	ft_putnbr(-530203234);
+	write(1, " ", 1);
 	ft_putnbr(-2147483648);
+	write(1, " ", 1);
 	ft_putnbr(2147483647);
 }
